850630ee8c4c554a5dc5053b1e0a13308a3e6112
vif <vif.ardente@gmail.com>
2019-10-18 12:59:43 +0300

    pooled binarytrees test is now based on GO#8

--- internal/tests/benchmarkgame-binarytrees/binarytrees.go
+++ internal/tests/benchmarkgame-binarytrees/binarytrees.go
@@ -1,119 +1,164 @@
-/* The Computer Language Benchmarks Game
- * http://benchmarksgame.alioth.debian.org/
- *
- * contributed by The Go Authors.
- * based on C program by Kevin Carson
- * flag.Arg hack by Isaac Gouy
- *
- * 2013-04
- * modified by Jamil Djadala to use goroutines
- *
- * 2016-07
- * modified by Sergey Philippov to use syscall.Mmap
- */
+// The Computer Language Benchmarks Game
+// http://benchmarksgame.alioth.debian.org/
+//
+// Go adaptation of binary-trees Rust #4 program
+// Use semaphores to match the number of workers with the CPU count
+//
+// contributed by Marcel Ibes
+//
+// modified by Sergey Philippov to use unmanaged memory pools
 
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"math"
 	"runtime"
+	"sort"
 	"strconv"
-	"sync"
 	"time"
+
+	"golang.org/x/sync/semaphore"
 
 	"github.com/pi/goal/gut"
 )
 
-var n = 20
-
-type Node struct {
-	item        int
-	left, right *Node
+type Tree struct {
+	Left  *Tree
+	Right *Tree
 }
 
-const bytesPerNode = 8 * 3
-
-func nodesPerTree(depth int) uint {
-	return (1<<uint(depth))*2 - 1
+type Message struct {
+	Pos  uint32
+	Text string
 }
 
-func bottomUpTree(p *gut.UnsafeMemoryPool, item, depth int) (n *Node) {
-	n = (*Node)(p.Alloc(bytesPerNode))
-	n.item = item
+type ByPos []Message
 
-	if depth > 0 {
-		n.left = bottomUpTree(p, 2*item-1, depth-1)
-		n.right = bottomUpTree(p, 2*item, depth-1)
-	} else {
-		n.left = nil
-		n.right = nil
+func (m ByPos) Len() int           { return len(m) }
+func (m ByPos) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+func (m ByPos) Less(i, j int) bool { return m[i].Pos < m[j].Pos }
+
+func itemCheck(tree *Tree) uint32 {
+	if tree.Left != nil && tree.Right != nil {
+		return uint32(1) + itemCheck(tree.Right) + itemCheck(tree.Left)
 	}
-	return
+
+	return 1
 }
 
-func (n *Node) itemCheck() int {
-	if n.left == nil {
-		return n.item
+func treeSize(depth uint32) uint {
+	return ((1<<uint(depth))*2 - 1) * 16
+}
+
+func bottomUpTree(pool *gut.UnsafeMemoryPool, depth uint32) *Tree {
+	tree := (*Tree)(pool.Alloc(16))
+	if depth > uint32(0) {
+		tree.Right = bottomUpTree(pool, depth-1)
+		tree.Left = bottomUpTree(pool, depth-1)
 	}
-	return n.item + n.left.itemCheck() - n.right.itemCheck()
+	return tree
 }
 
-const minDepth = 4
+func inner(depth, iterations uint32) string {
+	chk := uint32(0)
+	pool, _ := gut.NewUnsafeMemoryPool(treeSize(depth))
+	for i := uint32(0); i < iterations; i++ {
+		pool.Reset()
+		a := bottomUpTree(pool, depth)
+		chk += itemCheck(a)
+	}
+	return fmt.Sprintf("%d\t trees of depth %d\t check: %d", iterations, depth, chk)
+}
+
+const minDepth = uint32(4)
 
 func main() {
-	startTime := time.Now()
-
+	n := 21
 	flag.Parse()
 	if flag.NArg() > 0 {
 		n, _ = strconv.Atoi(flag.Arg(0))
 	}
 
+	run(uint32(n))
+}
+
+func run(n uint32) {
+	startTime := time.Now()
+
+	cpuCount := runtime.NumCPU()
+	sem := semaphore.NewWeighted(int64(cpuCount))
+
 	maxDepth := n
 	if minDepth+2 > n {
 		maxDepth = minDepth + 2
 	}
-	stretchDepth := maxDepth + 1
 
-	ltpool, _ := gut.NewUnsafeMemoryPool(nodesPerTree(stretchDepth) * bytesPerNode)
+	depth := maxDepth + 1
 
-	check_l := bottomUpTree(ltpool, 0, stretchDepth).itemCheck()
-	fmt.Printf("stretch tree of depth %d\t check: %d\n", stretchDepth, check_l)
-	ltpool.Reset()
+	messages := make(chan Message, cpuCount)
+	expected := uint32(2) // initialize with the 2 summary messages we're always outputting
 
-	longLivedTree := bottomUpTree(ltpool, 0, maxDepth)
+	go func() {
+		for halfDepth := minDepth / 2; halfDepth < maxDepth/2+1; halfDepth++ {
+			depth := halfDepth * 2
+			iterations := uint32(1 << (maxDepth - depth + minDepth))
+			expected++
 
-	var wg sync.WaitGroup
-	result := make([]string, maxDepth+1)
+			func(d, i uint32) {
+				if err := sem.Acquire(context.TODO(), 1); err == nil {
+					go func() {
+						defer sem.Release(1)
+						messages <- Message{d, inner(d, i)}
+					}()
+				} else {
+					panic(err)
+				}
+			}(depth, iterations)
+		}
 
-	gate := make(chan struct{}, runtime.NumCPU())
+		if err := sem.Acquire(context.TODO(), 1); err == nil {
+			go func() {
+				defer sem.Release(1)
+				pool, _ := gut.NewUnsafeMemoryPool(treeSize(depth))
+				tree := bottomUpTree(pool, depth)
+				messages <- Message{0,
+					fmt.Sprintf("stretch tree of depth %d\t check: %d", depth, itemCheck(tree))}
+				pool.Done()
+			}()
+		} else {
+			panic(err)
+		}
 
-	for depth_l := minDepth; depth_l <= maxDepth; depth_l += 2 {
-		gate <- struct{}{}
-		wg.Add(1)
-		go func(depth int, check int, r *string) {
-			defer wg.Done()
-			iterations := 1 << uint(maxDepth-depth+minDepth)
-			check = 0
+		if err := sem.Acquire(context.TODO(), 1); err == nil {
+			go func() {
+				defer sem.Release(1)
+				pool, _ := gut.NewUnsafeMemoryPool(treeSize(maxDepth))
+				longLivedTree := bottomUpTree(pool, maxDepth)
+				messages <- Message{math.MaxUint32,
+					fmt.Sprintf("long lived tree of depth %d\t check: %d", maxDepth, itemCheck(longLivedTree))}
+				pool.Done()
+			}()
+		} else {
+			panic(err)
+		}
+	}()
 
-			pool, _ := gut.NewUnsafeMemoryPool(nodesPerTree(depth) * bytesPerNode)
-			for i := 1; i <= iterations; i++ {
-				pool.Reset()
-				check += bottomUpTree(pool, i, depth).itemCheck()
-				pool.Reset()
-				check += bottomUpTree(pool, -i, depth).itemCheck()
-			}
-			pool.Done()
-			*r = fmt.Sprintf("%d\t trees of depth %d\t check: %d", iterations*2, depth, check)
-			<-gate
-		}(depth_l, check_l, &result[depth_l])
+	var sortedMsg []Message
+	for m := range messages {
+		sortedMsg = append(sortedMsg, m)
+		expected--
+		if expected == 0 {
+			close(messages)
+		}
 	}
-	wg.Wait()
-	for depth := minDepth; depth <= maxDepth; depth += 2 {
-		fmt.Println(result[depth])
+
+	sort.Sort(ByPos(sortedMsg))
+	for _, m := range sortedMsg {
+		fmt.Println(m.Text)
 	}
-	fmt.Printf("long lived tree of depth %d\t check: %d\n", maxDepth, longLivedTree.itemCheck())
-	ltpool.Done()
 
 	fmt.Printf("time spent: %v\n", time.Since(startTime))
 }
